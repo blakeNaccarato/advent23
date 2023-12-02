84bceaaeb995a24cf71e77055b437003f03d37e0
blakeNaccarato <blake.naccarato@gmail.com>
2023-12-02 14:15:47 -0800

    Solve day 1b

--- tests/advent23_tests/answers.py
+++ tests/advent23_tests/answers.py
@@ -1,3 +1,3 @@
 """Our answers to Advent of Code 2023."""
 
-ANSWERS = {"01": {"blake": (55621, None), "abdul": (54338, 53389)}}
+ANSWERS = {"01": {"blake": (55621, 53592), "abdul": (54338, 53389)}}
--- tests/advent23_tests/test_day01.py
+++ tests/advent23_tests/test_day01.py
@@ -1,7 +1,6 @@
 from advent23_tests import parametrize
 
 DAY = "01"
-
 USER = "abdul"
 
 
@@ -11,22 +10,10 @@ def test_abdul(ns, expected):
     assert ns.ANS == expected
 
 
-@parametrize(USER, DAY, reverse=True)
-def test_abdul_reverse(ns, expected):
-    """Tests for Abdul's puzzle answers."""
-    assert ns.ANS == expected
-
-
 USER = "blake"
 
 
 @parametrize(USER, DAY)
 def test_blake(ns, expected):
-    """Tests for Abdul's puzzle answers."""
-    assert ns.ANS == expected
-
-
-@parametrize(USER, DAY, reverse=True)
-def test_blake_reverse(ns, expected):
     """Tests for Abdul's puzzle answers."""
     assert ns.ANS == expected
