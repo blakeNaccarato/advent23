dac5cb5676ad9b8541b76c59147b925cf558a8ab
blakeNaccarato <blake.naccarato@gmail.com>
2023-12-01 22:12:03 -0800

    Blake: Solve day 1a

--- src/advent23/__init__.py
+++ src/advent23/__init__.py
@@ -1,1 +1,18 @@
 """Collaboration on Advent of Code 2023."""
+
+from typing import Any
+
+from IPython.core.display import Markdown
+from IPython.display import display
+
+
+def disp_names(*args: tuple[str, Any]):
+    """Display objects with names above them."""
+    for name, elem in args:
+        disp_name(name, elem)
+
+
+def disp_name(name: str, elem: Any):
+    """Display an object with its name above it."""
+    display(Markdown(f"#### {name}"))
+    display(elem)
--- tests/advent23_tests/answers.py
+++ tests/advent23_tests/answers.py
@@ -1,5 +1,3 @@
 """Correct answers to Advent of Code 2023."""
 
-ANSWERS = {
-    "01": (142, None)  # TODO: Replace `None` with the correct answer!
-}
+ANSWERS = {"01": (142, 55621)}
