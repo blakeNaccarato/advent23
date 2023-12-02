31075139f0c2164f33b4b2ac9cd7ae1b32e7925a
blakeNaccarato <blake.naccarato@gmail.com>
2023-12-01 18:48:11 -0800

    Test against the basic input regardless of the user's hardcoded INPUT variable

---
+++ src/advent23/_structured/__init__.py
@@ -0,0 +1,4 @@
+"""Like the template document, but with a little more structure to get you started.
+
+No spoilers, just a helping hand with a few function definitions and such.
+"""
--- tests/advent23_tests/answers.py
+++ tests/advent23_tests/answers.py
@@ -1,3 +1,5 @@
 """Correct answers to Advent of Code 2023."""
 
-ANSWERS = {"01": (142, None)}
+ANSWERS = {
+    "01": (142, None)  # TODO: Replace `None` with the correct answer!
+}
