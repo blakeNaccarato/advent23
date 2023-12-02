6cce83d5e54991efe56bed72b58cb549ef6283d4
blakeNaccarato <blake.naccarato@gmail.com>
2023-12-01 23:38:27 -0800

    Implement example tests

--- tests/advent23_tests/answers.py
+++ tests/advent23_tests/answers.py
@@ -1,4 +1,4 @@
 """Our answers to Advent of Code 2023."""
 
-BASIC_ANSWERS = {"01": {"a": 142, "b": None}}
+BASIC_ANSWERS = {"01": {"a": 142, "b": 281}}
 ANSWERS = {"01": {"blake": (55621, None), "abdul": (None, None)}}
