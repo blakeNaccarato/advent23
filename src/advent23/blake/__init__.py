b1e5ebf3e532c7b2743351500ba85070ad697065
blakeNaccarato <blake.naccarato@gmail.com>
2023-12-05 18:52:16 -0800

    Define Stringer

--- src/advent23/blake/__init__.py
+++ src/advent23/blake/__init__.py
@@ -1,7 +1,25 @@
 """Blake's solutions."""
 
+from collections.abc import Iterator, MutableMapping
 from re import MULTILINE, VERBOSE, Pattern, compile
 from string import Template
+
+
+class Stringer(MutableMapping[str, str]):
+    def __getitem__(self, __key: str) -> str:
+        return super().__getitem__(__key)
+
+    def __setitem__(self, key: str, pattern: str) -> None:
+        return super().__setitem__(key, pattern)
+
+    def __delattr__(self, __name: str) -> None:
+        return super().__delattr__(__name)
+
+    def __iter__(self) -> Iterator[str]:
+        return super().__iter__()
+
+    def __len__(self) -> int:
+        return super().__len__()
 
 
 def tsub(_root: str = "root", **patterns: str) -> str:
