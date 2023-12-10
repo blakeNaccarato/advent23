37fd50a072920e67ddce91f35044a053460656f3
blakeNaccarato <blake.naccarato@gmail.com>
2023-12-09 18:06:59 -0800

    Make Stringer recursively substitute sub-Stringers

--- src/advent23/blake/__init__.py
+++ src/advent23/blake/__init__.py
@@ -3,46 +3,48 @@
 from collections.abc import Iterator, MutableMapping
 from re import MULTILINE, VERBOSE, Pattern, compile
 from string import Template
-from types import SimpleNamespace
-
-from more_itertools import last
+from typing import Self
 
 
-class Stringer(MutableMapping[str, str]):
-    def __init__(self, root: str, **patterns: str) -> None:
-        self._templates: dict[str, str] = {"root": root} | patterns
-        self._root = root
-        self.ns = SimpleNamespace(**self._templates)
+class Stringer(MutableMapping[str, Self | str]):
+    def __init__(self, /, root: str, **patterns: Self | str) -> None:
+        self.root = root
+        self.patterns = {"root": root, **patterns}
         super().__init__()
 
-    def sub(self, **patterns) -> str:
-        """Substitute patterns until no more substitutions are possible."""
-        return last(
-            self.subber(self._root, **(self._templates | patterns)), default=self._root
-        )
+    def sub(self, **other_patterns: Self | str) -> str:
+        return tuple(self.subber(**other_patterns))[-1]
 
-    def subber(self, result: str, **patterns) -> Iterator[str]:
-        """Substitute patterns, yielding intermediate values."""
-        while result != (result := Template(result).substitute(patterns)):
+    def subber(self, **other_patterns: Self | str) -> Iterator[str]:
+        result = self.root
+        while result != (
+            result := Template(result).substitute(
+                {
+                    name: pattern if isinstance(pattern, str) else pattern.subber()
+                    for name, pattern in {**self.patterns, **other_patterns}.items()
+                }
+            )
+        ):
+            yield result
+        if result == self.root:
             yield result
 
-    def __getitem__(self, key: str) -> str:
-        return self._templates[key]
+    def __getitem__(self, key: str) -> Self | str:  # type: ignore
+        return self.patterns[key]
 
-    def __setitem__(self, key: str, pattern: str):
-        self._templates[key] = pattern
-        setattr(self.ns, key, pattern)
+    def __setitem__(self, key: str, pattern: Self | str):
+        self.patterns[key] = pattern
 
     def __delitem__(self, key: str):
-        return super().__delitem__(key)
+        del self.patterns[key]
 
     def __iter__(self) -> Iterator[str]:
-        return super().__iter__()
+        return iter(self.patterns)
 
     def __len__(self) -> int:
-        return len(self._templates)
+        return len(self.patterns)
 
 
 def rem(pattern: str) -> Pattern[str]:
-    """Multiline, verbose, compiled regex pattern with baked-in `^` and `$`."""
+    """Multiline, verbose, compiled regex pattern."""
     return compile(flags=VERBOSE | MULTILINE, pattern=f"^{pattern}$")
