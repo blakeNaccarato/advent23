fa4153837c8ee654ca082fb5acd68735560ae105
blakeNaccarato <blake.naccarato@gmail.com>
2023-12-12 17:25:18 -0800

    Allow selecting subset of tests by day as well as by user

--- scripts/personalize_tests.py
+++ scripts/personalize_tests.py
@@ -6,15 +6,11 @@ from textwrap import dedent
 from typer import run
 
 
-def main(user: str):
-    """Personalize tests for a given user.
-
-    Args:
-        user: The user to personalize the tests for.
-    """
-    user = user.casefold()
+def main(user: str, day: str):
+    user = "" if (user := user.casefold()) == "any" else user
+    day = "" if day.casefold() == "any" else day.zfill(2)
     path = Path("pytest.ini")
-    if user == "all":
+    if not user and not day:
         path.unlink(missing_ok=True)
         return
     path.write_text(
@@ -28,7 +24,7 @@ def main(user: str):
                 --color yes
                 -p no:legacypaths
                 -r a
-                -k {user}_
+                -k {user}_day{day}
             cache_dir = .cache/.pytest_cache
             markers = slow
             testpaths = tests
