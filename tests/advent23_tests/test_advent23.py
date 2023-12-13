8a3eea113455cc28754098b20413901967758bb9
blakeNaccarato <blake.naccarato@gmail.com>
2023-12-13 09:04:57 -0800

    Reduce tests

--- tests/advent23_tests/test_advent23.py
+++ tests/advent23_tests/test_advent23.py
@@ -18,15 +18,15 @@ def test_example(att: Attempt, check: str):
     assert att.get_answer(check) == att.get_expected_answer(check)
 
 
-@pytest.mark.parametrize(
-    ("att", "check"),
-    (
-        pytest.param(attempt, check, id=attempt.get_id(check, pos=pos))
-        for attempt in walk_attempts("blake")
-        for pos, check in enumerate(attempt.checks)
-        if attempt.user != "blake"
-    ),
-)
-def test_compare_to_blake(att: Attempt, check: str):
-    """Test common checkpoints between attempts and Blake's attempts."""
-    assert att.get_answer(check) == att.get_expected_answer(check)
+# @pytest.mark.parametrize(
+#     ("att", "check"),
+#     (
+#         pytest.param(attempt, check, id=attempt.get_id(check, pos=pos))
+#         for attempt in walk_attempts("blake")
+#         for pos, check in enumerate(attempt.checks)
+#         if attempt.user != "blake"
+#     ),
+# )
+# def test_compare_to_blake(att: Attempt, check: str):
+#     """Test common checkpoints between attempts and Blake's attempts."""
+#     assert att.get_answer(check) == att.get_expected_answer(check)
